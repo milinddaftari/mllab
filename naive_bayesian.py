50688b245a4f82e679d9d4227ab107b231f1485d
Milind Daftari <mlind007.md@gmail.com>
2018-10-29 11:25:15 +0530

    ML

---
+++ naive_bayesian.py
@@ -0,0 +1,43 @@
+import pandas as pd
+msg=pd.read_csv('naivetrext1.csv',names=['message','label'])
+print('The dimensions of the dataset',msg.shape)
+msg['labelnum']=msg.label.map({'pos':1,'neg':0})
+X=msg.message
+y=msg.labelnum
+#splitting the dataset into train and test data
+from sklearn.model_selection import train_test_split
+xtrain,xtest,ytrain,ytest=train_test_split(X,y)
+print("xtest.shape")
+print(xtest.shape)
+print("xtrain.shape")
+print(xtrain.shape)
+print(ytest.shape)
+print(ytrain.shape)
+#output of count vectoriser is a sparse matrix
+from sklearn.feature_extraction.text import CountVectorizer
+count_vect = CountVectorizer()
+xtrain_dtm = count_vect.fit_transform(xtrain)
+print(xtrain_dtm)
+xtest_dtm=count_vect.transform(xtest)
+print(count_vect.get_feature_names())
+df=pd.DataFrame(xtrain_dtm.toarray(),columns=count_vect.get_feature_names())
+print(df)#tabular representation
+print(xtrain_dtm) #sparse matrix representation
+# Training Naive Bayes (NB) classifier on training data.
+from sklearn.naive_bayes import MultinomialNB
+clf = MultinomialNB().fit(xtrain_dtm,ytrain)
+predicted = clf.predict(xtest_dtm)
+#printing accuracy metrics
+from sklearn import metrics
+print('Accuracy metrics')
+print('Accuracy of the classifer is',metrics.accuracy_score(ytest,predicted))
+print('Confusion matrix')
+print(metrics.confusion_matrix(ytest,predicted))
+print('Recall and Precison ')
+print(metrics.recall_score(ytest,predicted))
+print(metrics.precision_score(ytest,predicted))
+'''docs_new = ['I like this place', 'My boss is not my saviour']
+X_new_counts = count_vect.transform(docs_new)
+predictednew = clf.predict(X_new_counts)
+for doc, category in zip(docs_new, predictednew):
+print('%s->%s' % (doc, msg.labelnum[category]))'''
