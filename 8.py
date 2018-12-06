39830253dcf966af662e727922bbfbd231a4a2ce
Milind Daftari <mlind007.md@gmail.com>
2018-12-06 17:09:50 +0530

    update

--- 8.py
+++ 8.py
@@ -21,15 +21,13 @@ plt.show()
 kmeans = KMeans(2, random_state=0)
 labels = kmeans.fit(X).predict(X)
 centroids = kmeans.cluster_centers_
-plt.scatter(X[:, 0], X[:, 1], c=labels, s=40);
+plt.scatter(f1, f2, c=labels, s=40);
 print('Graph using Kmeans Algorithm')
 plt.scatter(centroids[:, 0], centroids[:, 1], marker='*', s=200, c='#050505')
 plt.show()
 #gmm demo
 gmm = GaussianMixture(n_components=2).fit(X)
 labels = gmm.predict(X)
-probs = gmm.predict_proba(X)
-size = 10 * probs.max(1) ** 3
 print('Graph using EM Algorithm')
-plt.scatter(X[:, 0], X[:, 1], c=labels, s=size, cmap='viridis')
+plt.scatter(f1,f2, c=labels)
 plt.show()
